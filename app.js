e4f86fed02d93eb556920bef233782f36a441a41
kylemorton5770 <michael.morton@smail.astate.edu>
2016-02-19 16:18:48 -0600

    added add/remove logic for parties-list

--- app.js
+++ app.js
@@ -10,14 +10,26 @@ if (Meteor.isClient){
 		templateUrl: 'parties-list.html',
 		controllerAs: 'partiesList',
 		controller: function($scope, $reactive) {
-			$reactive(this).attach($scope);
-	
-			this.helpers({
-				parties: function() {
-					return Parties.find({});
-				}
-			});
-		}
+				$reactive(this).attach($scope);
+				
+				this.newParty = {};
+		
+				this.helpers({
+					parties: function() {
+						return Parties.find({});
+					}
+				});
+				
+				this.addParty = function() {
+					Parties.insert(this.newParty);
+					this.newParty = {};
+				};
+				
+				this.removeParty = function(party){
+					Parties.remove({_id: party._id});	
+				};
+				
+			}
 		}
 	});
 	
