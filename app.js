445c9a7e7c320307e587c5cfc39bd58344b72426
kylemorton5770 <michael.morton@smail.astate.edu>
2016-02-19 16:32:11 -0600

    added link to party details page. using angular-ui ui-sref tag

--- app.js
+++ app.js
@@ -2,7 +2,24 @@ Parties = new Mongo.Collection("parties");
 
 //tells meteor -> only run this code on client side
 if (Meteor.isClient){
-	var app = angular.module('socially', ['angular-meteor']);
+	var app = angular.module('socially', ['angular-meteor', 'ui.router']);
+	
+	//configure routes
+	app.config(function ($urlRouterProvider, $stateProvider, $locationProvider) {
+		$locationProvider.html5Mode(true);
+	
+		$stateProvider
+		.state('parties', {
+			url: '/parties',
+			template: '<parties-list></parties-list>'
+		})
+		.state('partyDetails', {
+			url: '/parties/:partyId',
+			template: '<party-details></party-details>'
+		});
+	
+		$urlRouterProvider.otherwise("/parties");
+	});
 	
 	app.directive('partiesList', function() {
 		return {
@@ -29,6 +46,17 @@ if (Meteor.isClient){
 					Parties.remove({_id: party._id});	
 				};
 				
+			}
+		}
+	});
+	
+	app.directive('partyDetails', function() {
+		return {
+			restrict: 'E',
+			templateUrl: 'party-details.html',
+			controllerAs: 'partyDetails',
+			controller: function ($scope, $stateParams) {
+				this.partyId = $stateParams.partyId;
 			}
 		}
 	});
