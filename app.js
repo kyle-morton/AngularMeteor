68c152e83d4cd56557e0599f17c6f774ad9679de
kylemorton5770 <michael.morton@smail.astate.edu>
2016-02-19 15:52:45 -0600

    created sample front end with sample controller/app

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
 //tells meteor -> only run this code on client side
 if (Meteor.isClient){
-	angular.module('socially', ['angular-meteor']);
+	var app = angular.module('socially', ['angular-meteor']);
+	
+	app.controller('PartiesListCtrl', function ($scope) {
+		
+		$scope.parties = [
+		{
+			'name': 'Dubstep-Free Zone',
+			'description': 'Can we please just for an evening not listen to dubstep.'
+		},
+		{
+			'name': 'All dubstep all the time',
+			'description': 'Get it on!'
+		},
+		{
+			'name': 'Savage lounging',
+			'description': 'Leisure suit required. And only fiercest manners.'
+		}
+		];
+	});
+	
 }
