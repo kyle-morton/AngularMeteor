6c7cbf62e51248faaf196e133243e96e45667f0d
kylemorton5770 <michael.morton@smail.astate.edu>
2016-02-22 13:10:39 -0600

    adding routing to catch unauth users. bug now on redirect if accessing details page and not auth.

--- client/routes.js
+++ client/routes.js
@@ -9,8 +9,29 @@ angular.module('socially').config(function ($urlRouterProvider, $stateProvider, 
     })
     .state('partyDetails', {
       url: '/parties/:partyId',
-      template: '<party-details></party-details>'
+      template: '<party-details></party-details>',
+      resolve: {
+          currentUser: function($q) {
+            if (Meteor.userId() == null) {
+              return $q.reject('AUTH_REQUIRED');
+            }
+            else {
+              return $q.resolve();
+            }
+          }
+        }
     });
  
   $urlRouterProvider.otherwise("/parties");
+})
+
+.run(function($rootScope, $state){
+  $rootScope.$on('$stateChangeError', function (event, toState, toParams, fromState, fromParams, error) {
+      
+      console.log("ERROR: " + error);
+      if (error === 'AUTH_REQUIRED') {
+        //if not logged in, redirect back to parties (change to parties state)
+        $state.go('parties');
+      }
+    });
 });
